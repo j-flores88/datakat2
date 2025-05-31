ae41f067aa819868d639c127193af0303e1241e4
Joel Flores <j_flores88@outlook.com>
2025-05-31 08:41:14 -0700

    slide show for pics + new pics

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector('.navlist');
+const menu = document.querySelector("#menu-icon");
+const navlist = document.querySelector('.navlist');
+const track = document.querySelector('.carousel_track');
+
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
